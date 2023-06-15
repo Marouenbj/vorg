c0e4c7f223691173c2ad017027959c523dd75152
Marouenbj <75218803+Marouenbj@users.noreply.github.com>
2023-06-15 19:16:49 +0200

    Add files via upload

---
+++ src/LCFSPR.c
@@ -0,0 +1,24 @@
+#include "../lib/LCFSPR.h"
+
+static queue_object* LCFSPR_queue;
+//You can add more global variables here
+
+int LCFSPR_startup(){
+    // TODO
+    return 0;
+}
+
+process* LCFSPR_tick (process* running_process){
+    // TODO
+    return NULL;
+}
+
+
+process* LCFSPR_new_arrival(process* arriving_process, process* running_process){
+    // TODO
+    return NULL;
+}
+
+void LCFSPR_finish(){
+    // TODO
+}
---
+++ src/SJN.c
@@ -0,0 +1,23 @@
+#include "../lib/SJN.h"
+
+static queue_object* SJN_queue;
+//You can add more global variables here
+
+process* SJN_tick (process* running_process){
+    // TODO
+    return NULL;
+}
+
+int SJN_startup(){
+    // TODO
+    return 1;
+}
+
+process* SJN_new_arrival(process* arriving_process, process* running_process){
+    // TODO
+    return NULL;
+}
+
+void SJN_finish(){
+    // TODO
+}
